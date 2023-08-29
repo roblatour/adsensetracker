1b884957330c6fe4d263651dbb8463355a8d9f86
Rob Latour <5200730+roblatour@users.noreply.github.com>
2023-08-29 17:48:02 -0400

    Update esp32AdSenseTracker.ino
    Added logic to display device's IP address for five seconds when bottom button is pressed

--- Arduino/esp32AdSenseTracker/esp32AdSenseTracker.ino
+++ Arduino/esp32AdSenseTracker/esp32AdSenseTracker.ino
@@ -1,4 +1,4 @@
-// ESP32 AdSense Tracker v1.1
+// ESP32 AdSense Tracker v1.2
 //
 // Copyright Rob Latour, 2023
 // License: MIT
@@ -35,14 +35,17 @@
 #include "secrets.h"  // contains the Google, MQTT, Wi-Fi and Over The Air (OTA) update credentials used in this sketch
 
 #include <TFT_eSPI.h>       // for the LilyGo T-Display S3 use the TFT_eSPI library found here: https://github.com/Xinyuan-LilyGO/T-Display-S3/tree/main/lib
-#include <EspMQTTClient.h>  // https://github.com/bertmelis/espMqttClient
+#include <EspMQTTClient.h>  // https://github.com/plapointe6/EspMQTTClient
 #include <string.h>
 
 #define ProgramName "ESP32AdSenseTracker"
 
-// ESP32 LilyGo T-Display S3 Button stuff
-#define TOP_BUTTON 14  // Button marked 'Key' on LILYGO T-Display S3
-//#define BOTTOM_BUTTON 0   // Button marked 'BOT' on LILYGO T-Display S3; not used in this sketch
+// ESP32 Lilyo T-Display Green LED
+#define GREEN_LED 38
+
+// ESP32 LilyGo T-Display S3 Buttons
+#define TOP_BUTTON 14    // Button marked 'Key' on LILYGO T-Display S3
+#define BOTTOM_BUTTON 0  // Button marked 'BOT' on LILYGO T-Display S3; not used in this sketch
 
 // ESP32 LilyGo T-Display S3 Power On Pin (used to ensure the display is on when the device is running on battery)
 #define PIN_POWER_ON 15
@@ -61,6 +64,7 @@ String totalClicks = "0";
 String totalEarnings = "$0.00";
 
 TFT_eSPI tft = TFT_eSPI();
+
 #define TFT_TURQUOISE 0x2F5C  // http://www.barth-dev.de/online/rgb565-color-picker/
 
 int displayWidth = 320;
@@ -73,10 +77,11 @@ void setupSerialMonitor() {
   Serial.println("Starting " + String(ProgramName));
 }
 
-void setupButton() {
+void setupButtons() {
 
-  // setup button on LILYGO T-Display-S3
+  Serial.println("Setting up buttons");
   pinMode(TOP_BUTTON, INPUT_PULLUP);
+  pinMode(BOTTOM_BUTTON, INPUT_PULLUP);
 }
 
 bool checkButton(int pinNumber) {
@@ -102,15 +107,21 @@ bool checkButton(int pinNumber) {
 
 void setupDisplay() {
 
+  Serial.println("Setting up display");
+
   // turn on the T-Display screen when in battery mode
   pinMode(PIN_POWER_ON, OUTPUT);
   digitalWrite(PIN_POWER_ON, HIGH);
+
+  // turn off the green LED
+  pinMode(GREEN_LED, OUTPUT);
+  digitalWrite(GREEN_LED, LOW);
 
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE);
-
+  
   currentTextSize = 2;
   tft.setTextSize(currentTextSize);
   int tw = (displayWidth - (int)tft.textWidth(ProgramName)) / 2;
@@ -166,6 +177,31 @@ void updateDisplay() {
   tft.println(totalEarnings);
 };
 
+void DisplayTheIPAddress() {
+
+  int margin = 15;
+
+  int tw;
+  int fh = (int)tft.fontHeight();
+
+  int leftMargin = margin;
+  int midHeight = (displayHeight - fh) / 2;
+
+  String devicesIPAddress = WiFi.localIP().toString().c_str();
+
+  // clear the screen
+  tft.fillScreen(TFT_BLACK);
+
+  // display the IP Address
+  tft.setTextColor(TFT_WHITE);
+
+  tft.setCursor(leftMargin, midHeight - fh);
+  tft.println("IP address:");
+
+  tft.setCursor(leftMargin, midHeight + fh);
+  tft.println(devicesIPAddress);
+}
+
 void onConnectionEstablished() {
 
   client.subscribe("adsense/TotalPageViews", [](const String &payload) {
@@ -186,13 +222,9 @@ void onConnectionEstablished() {
   client.publish("adsense/refreshrequest", "get last known values");
 }
 
-void setup() {
+void setupWifi() {
 
-  setupSerialMonitor();
-
-  setupButton();
-
-  setupDisplay();
+  Serial.println("Setting up WiFi");
 
   // Enable debugging messages sent to serial output
   client.enableDebuggingMessages();
@@ -204,11 +236,24 @@ void setup() {
   onConnectionEstablished();
 }
 
+void setup() {
+
+  setupSerialMonitor();
+  setupDisplay();
+  setupButtons();
+  setupWifi();
+}
+
 void loop() {
 
   client.loop();
 
   if (checkButton(TOP_BUTTON))
-      client.publish("adsense/refreshrequest", "get current values from Google");
-      
+    client.publish("adsense/refreshrequest", "get current values from Google");
+
+  if (checkButton(BOTTOM_BUTTON)) {
+    DisplayTheIPAddress();
+    delay(5000);
+    updateDisplay();
+  };
 }
